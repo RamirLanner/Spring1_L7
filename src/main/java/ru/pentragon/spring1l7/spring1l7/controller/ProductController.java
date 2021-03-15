29e0d67b4a1b38704a398d3f5ce015240c853d2c
RamirLanner <pentragon@yandex.ru>
2021-03-16 01:24:55 +0300

    hw8

--- src/main/java/ru/pentragon/spring1l7/spring1l7/controller/ProductController.java
+++ src/main/java/ru/pentragon/spring1l7/spring1l7/controller/ProductController.java
@@ -1,10 +1,14 @@
 package ru.pentragon.spring1l7.spring1l7.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.pentragon.spring1l7.spring1l7.model.Product;
 import ru.pentragon.spring1l7.spring1l7.services.ProductService;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -14,9 +18,27 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+
+    @Transactional
+    public List<Product> getAll(int page, int size, Sort.Direction sortCost, Sort.Direction sortName){
+        return productService.getAll(page-1, size,sortCost, sortName);
+    }
+
+    //http://localhost:8189/app/products?page=1&size=21&sortCost=DESC&sortName=DESC
     @GetMapping
-    public List<Product> getAll(){
-        return productService.getAll();
+    public ResponseEntity<Object> returnAll(@RequestParam(defaultValue = "1") Integer page,
+                                            @RequestParam(defaultValue = "10") Integer size,
+                                            @RequestParam(defaultValue = "ASC") String sortCost,
+                                            @RequestParam(defaultValue = "ASC") String sortName){
+        Sort.Direction sortCostDir = Sort.Direction.ASC;
+        Sort.Direction sortNameDir = Sort.Direction.ASC;
+        if(sortCost.equals("DESC")) sortCostDir = Sort.Direction.DESC;
+        if(sortName.equals("DESC")) sortNameDir = Sort.Direction.DESC;
+        List<Product> prod = getAll(page, size,sortCostDir,sortNameDir);
+        if (prod.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        return ResponseEntity.ok(prod);//getAll(page, size);
     }
 
     @GetMapping("/{id}")
--- src/main/java/ru/pentragon/spring1l7/spring1l7/services/ProductService.java
+++ src/main/java/ru/pentragon/spring1l7/spring1l7/services/ProductService.java
@@ -1,6 +1,8 @@
 package ru.pentragon.spring1l7.spring1l7.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.pentragon.spring1l7.spring1l7.model.Product;
 import ru.pentragon.spring1l7.spring1l7.repository.ProductRepository;
@@ -15,6 +17,12 @@ public class ProductService {
 
     public List<Product> getAll(){
         return productrRepository.findAll();
+    }
+
+    public List<Product> getAll(int page, int size, Sort.Direction sortCost, Sort.Direction sortName){
+        return productrRepository.findAll(PageRequest.of(page, size
+                , Sort.by(sortCost,"cost").and(Sort.by(sortName,"title")))).toList();
+        //return productrRepository.findAll(PageRequest.of(page, size)).toList();
     }
 
     public Product getProductByID(Long id){
